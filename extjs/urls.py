ec34e04b74da60f947a8d6a926cd7deeb18b632f
mahongquan <mahongquan@sina.com>
2016-05-29 08:03:33 +0800

    new merge

---
+++ extjs/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from extjs import views
+urlpatterns = [
+        url(r'^$',views.index),
+        url(r'^ch11', views.ch11),
+        url(r'^board/$',views.board),
+        url(r'^board/(?P<id>\d+)$', views.boardOne),
+]
