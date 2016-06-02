cb14ef49b9f1cd90d74b83ff65bf6d351dda9986
mahongquan <mahongquan@sina.com>
2016-06-02 13:33:00 +0800

    search yiqibh

--- mysite/parts/admin.py
+++ mysite/parts/admin.py
@@ -35,7 +35,7 @@ class ContactAdmin(admin.ModelAdmin):
     date_hierarchy = 'tiaoshi_date'
     list_display =  ('hetongbh','yiqibh','yiqixinghao', 'yonghu')
     list_filter = ('baoxiang','yiqixinghao')
-    search_fields = ('hetongbh', 'yonghu')
+    search_fields = ('hetongbh', 'yonghu','yiqibh')
     list_per_page=10
 admin.site.register(Contact,ContactAdmin)
 
