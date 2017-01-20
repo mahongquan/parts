68e75f96da5c764f479f74b89b2fb9f6f3c687d0
mahongquan <mahongquan@sina.com>
2017-01-20 15:55:42 +0800

    open folder

--- sql.py
+++ sql.py
@@ -4,22 +4,21 @@ import sqlite3
 conn = sqlite3.connect('data.sqlite')
 c = conn.cursor()
 
-# c.execute('select * from parts_packitem')
+c.execute('select * from parts_contact order by yiqibh')
 # items=[]
-# for row in c:
-#     if row[1]!=4:
-#     	items.append(row)
-# for item in items:
-# 	c.execute("""insert into parts_packitem(pack_id,item_id,ct) values (9,%s,%s)""" % (item[2],item[3]))
-# conn.commit()
+for row in c:
+	s=""
+	for r in row:
+		s+=str(r)+'\t'
+	print(s)
 
 # # We can also close the cursor if we are done with it
 # c.close()
-cmds=codecs.open("new1.sql","r",'utf-8').read().split(";")
-for cmd in cmds:
-	try:
-		print(cmd)
-		c.execute(cmd)
-	except:
-		pass
-conn.commit()
+# cmds=codecs.open("new1.sql","r",'utf-8').read().split(";")
+# for cmd in cmds:
+# 	try:
+# 		print(cmd)
+# 		c.execute(cmd)
+# 	except:
+# 		pass
+# conn.commit()
