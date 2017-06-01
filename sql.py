5bc7272469aaba8b6b558b938eadbebd9d037168
mahongquan <mahongquan@sina.com>
2017-06-01 09:33:47 +0800

    sql.py“

--- sql.py
+++ sql.py
@@ -1,24 +1,38 @@
 # -*- coding: utf-8 -*-
 import codecs
 import sqlite3
+import traceback
 conn = sqlite3.connect('data.sqlite')
 c = conn.cursor()
 
-c.execute('select * from parts_contact order by yiqibh')
-# items=[]
-for row in c:
-	s=""
-	for r in row:
-		s+=str(r)+'\t'
-	print(s)
+# c.execute('select * from parts_contact order by yiqibh')
+# # items=[]
+# for row in c:
+# 	s=""
+# 	for r in row:
+# 		s+=str(r)+'\t'
+# 	print(s)
 
 # # We can also close the cursor if we are done with it
 # c.close()
-# cmds=codecs.open("new1.sql","r",'utf-8').read().split(";")
-# for cmd in cmds:
-# 	try:
-# 		print(cmd)
-# 		c.execute(cmd)
-# 	except:
-# 		pass
-# conn.commit()
+def remove(cmd):
+	cs=cmd.split("\n")
+	r=""
+	for c in cs:
+		if c[:2]=="--":
+			pass
+		else:
+			r+=c+"\n"
+	return r
+cmds=codecs.open("tableStruct.sql","r",'utf-8').read().split(";")
+for cmd in cmds:
+	try:
+		print(remove(cmd))
+		c.execute(cmd)
+	except sqlite3.OperationalError as e:
+		traceback.print_exc()
+		if "already exists" in str(e):
+			pass
+		else:
+			input("pause")
+conn.commit()
