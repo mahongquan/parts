209d24b23cce557fd4a33bc3ff22ebf9ac56cee1
mahongquan <mahongquan@sina.com>
2017-06-22 09:26:02 +0800

    upload standard

--- rest/views.py
+++ rest/views.py
@@ -1228,37 +1228,47 @@ def readStandardFile(fn,filename):
                 onedan.append(cells)
             else:
                 pass
+    rs=[]
     for one in dan:
-        treatOne(one,filename)   
+        r=treatOne(one,filename)
+        if r!=None: 
+            rs.append(r)
+    return rs
 def treatOne(rows,fn):
+    r=None
     beizhu=rows[1][7]
     if beizhu[:2]=="CS" or beizhu[:2]=="ON":
-        try:
-            d=Pack.objects.get(name=rows[0][1])
-        except ObjectDoesNotExist as e2:
+        name=rows[1][7]+"_"+fn
+        d=Pack.objects.filter(name=name)
+        logging.info(d)
+        if len(d)>0:
+            pass
+        else:
             d=Pack()
-        d.name=rows[1][7]+"_"+fn
-        d.save()
-        n=len(rows)
-        items=rows[4:4+n-4-3]
-        for i in items:
-            #i=DanjuItem()
-            print(i[1],i[2],i[3],i[4],i[5])
-            items=Item.objects.filter(bh=i[1]).all()
-            if len(items)>1:
-                item=items[0]
-            else:
-                item=Item()
-            item.bh=i[1]
-            item.name=str(i[2])+" "+str(i[1])
-            item.guige=i[3]
-            item.danwei=i[4]
-            item.save()
-            di=PackItem()
-            di.pack=d
-            di.item=item
-            di.ct=i[5]
-            di.save()
+            d.name=rows[1][7]+"_"+fn
+            d.save()
+            n=len(rows)
+            items=rows[4:4+n-4-3]
+            for i in items:
+                #i=DanjuItem()
+                print(i[1],i[2],i[3],i[4],i[5])
+                items=Item.objects.filter(bh=i[1]).all()
+                if len(items)>1:
+                    item=items[0]
+                else:
+                    item=Item()
+                item.bh=i[1]
+                item.name=str(i[2])+" "+str(i[1])
+                item.guige=i[3]
+                item.danwei=i[4]
+                item.save()
+                di=PackItem()
+                di.pack=d
+                di.item=item
+                di.ct=i[5]
+                di.save()
+            r={"id":d.id,"name":d.name}
+    return r
 def standard(request):
     # right, so 'file' is the name of the file upload field
     #print request.FILES
@@ -1268,5 +1278,5 @@ def standard(request):
     filename = f.name
     filetype = f.content_type
     packs=readStandardFile(f.read(),filename)
-    res={"success":True, "result":""}
+    res={"success":True, "result":packs}
     return HttpResponse(json.dumps(res, ensure_ascii=False))        
