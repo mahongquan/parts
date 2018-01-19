0392aadbb50842d437f807297d99bfd046c8dfe4
mahongquan <mahongquan@sina.com>
2018-01-19 11:35:52 +0800

    wsgiserver

---
+++ s1.py
@@ -0,0 +1,10 @@
+import wsgiserver
+from mysite import wsgi
+# def my_app(environ, start_response):
+#     status = '200 OK'
+#     response_headers = [('Content-type','text/plain')]
+#     start_response(status, response_headers)
+#     return ['WSGIserver is running!']
+
+server = wsgiserver.WSGIServer(wsgi.application, host='0.0.0.0', port=8000)
+server.start()
