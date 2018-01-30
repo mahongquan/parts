282c30b82b90100a218aed6e3b089e400f1e8717
Your Name <you@example.com>
2018-01-30 11:27:46 +0800

    example ok

--- static/mobx-react-boilerplate/src/index.js
+++ static/mobx-react-boilerplate/src/index.js
@@ -1,28 +1,48 @@
-import React from "react";
-import { render } from "react-dom";
-import DevTools from "mobx-react-devtools";
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { observable, action, computed } from "mobx";
+import { observer } from "mobx-react";
+class Todo {
+    id = Math.random();
+    @observable title = "";
+    @observable finished = false;
+    constructor(t){
+        this.title=t;
+    }
+}
+class TodoList {
+    @observable todos = [];
+    @computed get unfinishedTodoCount() {
+        return this.todos.filter(todo => !todo.finished).length;
+    }
+}
+@observer
+class TodoListView extends Component {
+    render() {
+        return <div>
+            <ul>
+                {this.props.todoList.todos.map(todo =>
+                    <TodoView todo={todo} key={todo.id} />
+                )}
+            </ul>
+            Tasks left: {this.props.todoList.unfinishedTodoCount}
+        </div>
+    }
+}
 
-import TodoList from "./components/TodoList";
-import TodoListModel from "./models/TodoListModel";
-import TodoModel from "./models/TodoModel";
+const TodoView = observer(({todo}) =>
+    <li>
+        <input
+            type="checkbox"
+            checked={todo.finished}
+            onClick={() => todo.finished = !todo.finished}
+        />{todo.title}
+    </li>
+)
 
-const store = new TodoListModel();
-
-render(
-  <div>
-    <DevTools />
-    <TodoList store={store} />
-  </div>,
-  document.getElementById("root")
+const store = new TodoList();
+store.todos.push(
+    new Todo("Get Coffee"),
+    new Todo("Write simpler code")
 );
-
-store.addTodo("Get Coffee");
-store.addTodo("Write simpler code");
-store.todos[0].finished = true;
-
-setTimeout(() => {
-  store.addTodo("Get a cookie as well");
-}, 2000);
-
-// playing around in the console
-window.store = store;
+ReactDOM.render(<TodoListView todoList={store} />, document.getElementById('root'));
