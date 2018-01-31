ac40560ed98c71bf8775dfe034911a7236ee2d6e
Your Name <you@example.com>
2018-01-31 09:28:48 +0800

    route

--- static/redux1/src/index.js
+++ static/redux1/src/index.js
@@ -1,15 +1,14 @@
 import ReactDOM from 'react-dom';
-import  Items     from './Items';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import  React   from 'react';
-import ItemEdit from './ItemEdit';
-
-class App extends React.Component {
-	render=()=>{
-		return(<div><ItemEdit ref="dlg" parent={this} /><Items /><Items /></div>);
-	}
-}
+//import ItemEdit from './ItemEdit';
+import App from './App_bootstrap_route';
+// class App extends React.Component {
+// 	render=()=>{
+// 		return(<div><ItemEdit ref="dlg" parent={this} /><Items /><Items /></div>);
+// 	}
+// }
 ReactDOM.render(
   <App />, 
   document.getElementById('root')
