30f060ce0474f46a7bf105f49ba64990b34e4e50
Your Name <you@example.com>
2018-04-10 14:37:10 +0800

    allow hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w%fdnf8bq5yygdgq*jvs)3bjqr+x_1&e+lgfx_%(8ud89n#4we'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.3.33","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["*","localhost","127.0.0.1"]
 
 
 # Application definition
