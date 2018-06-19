780ef0669d3f90363ca74bd4a92ae4f3e2745cb4
mahongquan <mahongquan@sina.com>
2018-06-19 11:39:20 +0800

    detail

--- tableStruct.sql
+++ tableStruct.sql
@@ -27,29 +27,6 @@ CREATE TABLE django_content_type (
 );
 
 
--- Table: parts_contact
-CREATE TABLE parts_contact ( 
-    id             INTEGER         PRIMARY KEY
-                                   NOT NULL,
-    yonghu         VARCHAR( 30 )   NOT NULL,
-    yiqixinghao    VARCHAR( 30 )   NOT NULL,
-    yiqibh         VARCHAR( 30 )   NOT NULL
-                                   UNIQUE,
-    baoxiang       VARCHAR( 30 )   NOT NULL,
-    shenhe         VARCHAR( 30 )   NOT NULL,
-    yujifahuo_date DATE            NOT NULL,
-    hetongbh       VARCHAR( 30 )   NOT NULL,
-    addr           VARCHAR( 30 ),
-    channels       VARCHAR( 30 ),
-    tiaoshi_date   DATE,
-    method         VARCHAR( 200 ),
-    dianqi         VARCHAR( 20 ),
-    jixie          VARCHAR( 30 ),
-    hongwai        VARCHAR( 20 ),
-    redao          VARCHAR( 20 ) 
-);
-
-
 -- Table: auth_permission
 CREATE TABLE auth_permission ( 
     id              INTEGER         NOT NULL
@@ -132,28 +109,6 @@ CREATE TABLE django_migrations (
 );
 
 
--- Table: extjs_ch11
-CREATE TABLE extjs_ch11 ( 
-    id     INTEGER        NOT NULL
-                          PRIMARY KEY AUTOINCREMENT,
-    name   VARCHAR( 30 )  NOT NULL,
-    gender VARCHAR( 1 )   NOT NULL,
-    dob    DATE           NOT NULL,
-    epaper BOOL           NOT NULL 
-);
-
-
--- Table: parts_pack_contact
-CREATE TABLE parts_pack_contact ( 
-    id         INTEGER NOT NULL
-                       PRIMARY KEY AUTOINCREMENT,
-    pack_id    INTEGER NOT NULL,
-    contact_id INTEGER NOT NULL
-                       REFERENCES parts_contact ( id ),
-    UNIQUE ( pack_id, contact_id ) 
-);
-
-
 -- Table: parts_pack
 CREATE TABLE parts_pack ( 
     id   INTEGER        NOT NULL
@@ -193,80 +148,39 @@ CREATE TABLE django_admin_log (
 );
 
 
--- Table: parts_danju
-CREATE TABLE parts_danju ( 
-    id         INTEGER        PRIMARY KEY
-                              NOT NULL,
-    danjuhao   VARCHAR( 30 )  NOT NULL,
-    danju_date DATE           NOT NULL,
-    cangku     VARCHAR( 30 )  NOT NULL,
-    bumeng     VARCHAR( 30 )  NOT NULL,
-    gongying   VARCHAR( 30 ),
-    shenhe     DATE,
-    leibie     VARCHAR( 30 ),
-    beizhu     VARCHAR( 30 ),
-    filename   VARCHAR( 30 ),
-    zhidan     VARCHAR( 30 ),
-    qianzi     VARCHAR( 30 ) 
-);
-
-
--- Table: parts_danjuitem
-CREATE TABLE parts_danjuitem ( 
-    id       INTEGER NOT NULL
-                     PRIMARY KEY AUTOINCREMENT,
-    danju_id INTEGER NOT NULL
-                     REFERENCES parts_pack ( id ),
-    item_id  INTEGER NOT NULL
-                     REFERENCES parts_item ( id ),
-    ct       INTEGER NOT NULL 
-);
-
-
 -- Table: parts_packitem
 CREATE TABLE parts_packitem ( 
-    id      INTEGER PRIMARY KEY AUTOINCREMENT
-                    NOT NULL,
+    id      INTEGER NOT NULL
+                    PRIMARY KEY AUTOINCREMENT,
     pack_id INTEGER NOT NULL
                     REFERENCES parts_pack ( id ),
     item_id INTEGER NOT NULL
                     REFERENCES parts_item ( id ),
-    ct      REAL    NOT NULL,
+    ct      INTEGER NOT NULL,
     quehuo  BOOLEAN 
 );
 
 
--- Table: parts2_pack
-CREATE TABLE parts2_pack ( 
-    id   INTEGER        NOT NULL
-                        PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR( 30 )  NOT NULL 
-);
-
-
--- Table: parts2_item
-CREATE TABLE parts2_item ( 
-    id      INTEGER         PRIMARY KEY
-                            NOT NULL,
-    bh      VARCHAR( 30 ),
-    name    VARCHAR( 30 )   NOT NULL,
-    guige   VARCHAR( 30 ),
-    ct      INTEGER         NOT NULL,
-    danwei  VARCHAR( 30 )   NOT NULL,
-    image   VARCHAR( 100 ),
-    name_en CHAR( 30 ),
-    beizhu  CHAR( 30 ) 
-);
-
-
--- Table: parts_usepack2
-CREATE TABLE parts_usepack2 ( 
-    id         INTEGER PRIMARY KEY AUTOINCREMENT
-                       NOT NULL,
-    contact_id INTEGER NOT NULL
-                       REFERENCES parts_contact ( id ),
-    pack_id    INTEGER NOT NULL
-                       REFERENCES parts2_pack 
+-- Table: parts_contact
+CREATE TABLE parts_contact ( 
+    id             INTEGER         PRIMARY KEY
+                                   NOT NULL,
+    yonghu         VARCHAR( 30 )   NOT NULL,
+    yiqixinghao    VARCHAR( 30 )   NOT NULL,
+    yiqibh         VARCHAR( 30 )   NOT NULL,
+    baoxiang       VARCHAR( 30 )   NOT NULL,
+    shenhe         VARCHAR( 30 )   NOT NULL,
+    yujifahuo_date DATE            NOT NULL,
+    hetongbh       VARCHAR( 30 )   NOT NULL,
+    addr           VARCHAR( 30 ),
+    channels       VARCHAR( 30 ),
+    tiaoshi_date   DATE,
+    method         VARCHAR( 200 ),
+    redao          VARCHAR( 20 ),
+    hongwai        VARCHAR( 20 ),
+    jixie          VARCHAR( 30 ),
+    dianqi         VARCHAR( 20 ),
+    detail         TEXT 
 );
 
 
@@ -323,18 +237,6 @@ CREATE INDEX django_session_3da3d3d8 ON django_session (
 );
 
 
--- Index: parts_pack_contact_9391bab4
-CREATE INDEX parts_pack_contact_9391bab4 ON parts_pack_contact ( 
-    pack_id 
-);
-
-
--- Index: parts_pack_contact_816533ed
-CREATE INDEX parts_pack_contact_816533ed ON parts_pack_contact ( 
-    contact_id 
-);
-
-
 -- Index: auth_permission_417f1b1c
 CREATE INDEX auth_permission_417f1b1c ON auth_permission ( 
     content_type_id 
@@ -380,18 +282,6 @@ CREATE INDEX parts_packitem_9391bab4 ON parts_packitem (
 -- Index: parts_packitem_0a47aae8
 CREATE INDEX parts_packitem_0a47aae8 ON parts_packitem ( 
     item_id 
-);
-
-
--- Index: parts_contactpack_816533ed
-CREATE INDEX parts_contactpack_816533ed ON parts_usepack2 ( 
-    contact_id 
-);
-
-
--- Index: parts_contactpack_9391bab4
-CREATE INDEX parts_contactpack_9391bab4 ON parts_usepack2 ( 
-    pack_id 
 );
 
 
