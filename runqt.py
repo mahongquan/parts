10d0697d6c8011b2784094a11af331f91936dbcc
mahongquan <mahongquan@sina.com>
2018-06-29 08:39:10 +0800

    restore qt5

---
+++ runqt.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import codecs
+#import django
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#django.setup()
+from qt5_client import contacts,login
+contacts.main()
