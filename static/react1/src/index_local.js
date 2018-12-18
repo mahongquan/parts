069dd6c95beb25a485811f1c3ebb03fed4f2c90c
mahongquan <mahongquan@sina.com>
2018-12-18 11:54:50 +0800

    index_local

--- static/react1/src/index_local.js
+++ static/react1/src/index_local.js
@@ -1,26 +1,26 @@
 console.log("index_local");
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// const fs = window.require('fs');
-// const path = window.require('path');
-// function fileExist(p) {
-//   if (fs.existsSync(p)) {
-//     return true;
-//   }
-//   return false;
-// }
-// function link(where, module_name) {
-//   // body...
-//   var thelink = document.createElement('link');
-//   thelink.setAttribute('rel', 'stylesheet');
-//   var file1 = path.join(where, module_name);
-//   thelink.setAttribute('href', file1);
-//   document.head.appendChild(thelink);
-// }
-// let where = window.require('electron').ipcRenderer.sendSync('getpath');
-// let module_name = './App_bootstrap';
-// link(where, 'node_modules/bootstrap/dist/css/bootstrap.min.css');
-// link('./', 'autosuggest.css');
-// link(where, 'node_modules/react-datetime/css/react-datetime.css');
-// let App = require(module_name).default;
-// ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+const fs = window.require('fs');
+const path = window.require('path');
+function fileExist(p) {
+  if (fs.existsSync(p)) {
+    return true;
+  }
+  return false;
+}
+function link(where, module_name) {
+  // body...
+  var thelink = document.createElement('link');
+  thelink.setAttribute('rel', 'stylesheet');
+  var file1 = path.join(where, module_name);
+  thelink.setAttribute('href', file1);
+  document.head.appendChild(thelink);
+}
+let where = window.require('electron').ipcRenderer.sendSync('getpath');
+let module_name = './App_bootstrap';
+link(where, 'node_modules/bootstrap/dist/css/bootstrap.min.css');
+link('./', 'autosuggest.css');
+link(where, 'node_modules/react-datetime/css/react-datetime.css');
+let App = require(module_name).default;
+ReactDOM.render(<App />, document.getElementById('root'));
