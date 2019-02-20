0829c0787a9c70308fb0a93d5efecb2bfd0b5676
mahongquan <mahongquan@sina.com>
2019-02-20 20:49:17 +0800

    rm contacts

--- static/mobx-react-boilerplate/src/index.js
+++ static/mobx-react-boilerplate/src/index.js
@@ -1,12 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-// import Contacts from './AppContacts';
-import Contacts from './todos2/index3.js';
-// import Toolbar from './AppToolbar'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './autosuggest.css';
-import 'react-datetime/css/react-datetime.css';
+import Contacts from './todos2/index';
 ReactDOM.render(
     <div>
     <Contacts />
