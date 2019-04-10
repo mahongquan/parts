f4b599390aa07260877c73b82c3c3e20df686d72
Your Name <you@example.com>
2019-04-10 13:11:57 +0800

    bs4

--- static/react1/src/index.js
+++ static/react1/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './autosuggest.css';
 import 'react-datetime/css/react-datetime.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import './bs4/index.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import './bs4/index.css';
+
 import myglobal from './myglobal';
 myglobal.api = 'fetch';
 myglobal.Client = require('./Client_fetch').default;
-var App = require('./bs4/App_bootstrap').default;
+var App = require('./mui/App').default;
 ReactDOM.render(<App />, document.getElementById('root'));
