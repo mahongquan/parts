64b14149ba39fd69dea2de5f8897ccbb99256bd2
Your Name <you@example.com>
2019-05-21 07:58:20 +0800

    add

--- static/systemjs/hello.js
+++
@@ -1,6 +0,0 @@
-// hello.mjs
-export function hello(text) {
-  const div = document.createElement('div');
-  div.textContent = `Hello ${text}`;
-  document.body.appendChild(div);
-}
--- static/systemjs/main.js
+++
@@ -1,4 +0,0 @@
-import { getUsefulContents } from './file.js';
-
-getUsefulContents('http://www.baidu.com',
-    data => { doSomethingUseful(data); });
