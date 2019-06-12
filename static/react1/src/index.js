67fd656cbc1825b75a4df53c27afc609f7d2c867
Your Name <you@example.com>
2019-06-12 11:25:46 +0800

    react-select

--- static/react1/src/index.js
+++ static/react1/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './autosuggest.css';
+import App from './Select1.js'
 // import 'bootstrap/dist/css/bootstrap.css';
 // import './bs4/index.css';
-var App = require('./mui/App').default;
+// var App = require('./mui/App').default;
+// var App = require('./Select0.js').default;
 // var App = require('./MPicker').default;
 ReactDOM.render(<App />, document.getElementById('root'));
