38bfdf462c9d7ae00ed66bca9bc43704ee7fce5c
Your Name <you@example.com>
2019-06-14 13:05:48 +0800

    recharts

--- static/react1/src/index.js
+++ static/react1/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './autosuggest.css';
-import App from './mui/App.js'
+// import App from './rechart1.js'
+import App from './mui/DlgStat3'
 // import App from './mui/SelectYQXH.js'
 // import App from './SelectA'
 // import App from './SelectPack.js'
