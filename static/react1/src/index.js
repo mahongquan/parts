8d57ae6252530d7987a4601c513f4e17372f1cf0
Your Name <you@example.com>
2019-10-17 10:16:47 +0800

    why rechart crash

--- static/react1/src/index.js
+++ static/react1/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './bs4/App'
-import 'bootstrap/dist/css/bootstrap.css';
-import './autosuggest.css';
-import './react-datetime.css';
-import './bs4/index.css';
+import App from './indexRouter'
+// import 'bootstrap/dist/css/bootstrap.css';
+// import './autosuggest.css';
+// import './react-datetime.css';
+// import './bs4/index.css';
 ReactDOM.render(<App />, document.getElementById('root'));
