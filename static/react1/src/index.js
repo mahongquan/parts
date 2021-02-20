99f9201d096f8a1654f3baa92b8eaf681a8b0cdd
mahongquan <mahongquan@sina.com>
2021-02-21 07:43:48 +0800

    h

--- static/react1/src/index.js
+++ static/react1/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './mui_redux/index';
 import * as serviceWorker from './serviceWorker';
-import './autosuggest.css';
+// import './autosuggest.css';
 import App from './MyInput';
 ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
